fb135ff689e1f7f8f049b91b10400cf861498da6
PREM KUMAR NARAYANAPPA VENKATESH <premkumarnarayanappavenkatesh@PREMs-MacBook-Air.local>
2018-10-04 23:59:16 -0400

    Initial commit with basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { RecipeComponent } from './recipe/recipe.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { RecipeListComponent } from './recipe/recipe-list/recipe-list.component';
+import { RecipeDetailsComponent } from './recipe/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipe/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping/shopping-list/shopping-list.component';
+import { ShoppingListEditComponent } from './shopping/shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeComponent,
+    ShoppingComponent,
+    RecipeListComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingListEditComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/shopping/shopping.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector : 'app-shopping',
+    templateUrl : './shopping.component.html',
+    styleUrls : ['./shopping.component.css']
+})
+export class ShoppingComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() {
+    }
+}
